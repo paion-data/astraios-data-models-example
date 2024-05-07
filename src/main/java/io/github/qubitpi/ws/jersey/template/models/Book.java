4ed43acecd3e3bb5fe800819833523f333e183ba
Jiaqi Liu <jack20220723@gmail.com>
2024-05-07 18:28:51 +0800

    Publish example data model to maven central (#10)

--- src/main/java/io/github/qubitpi/ws/jersey/template/models/Book.java
+++ src/main/java/io/github/qubitpi/ws/jersey/template/models/Book.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.qubitpi.ws.jersey.template.models;
+package io.github.qubitpi.ws.jersey.template.models;
 
 import com.yahoo.elide.annotation.Include;
 
